e18a20fba96a58c754ce993a9d4a2eb654161651
Ivan Shumilov <ivanshum@mustbefamily.com>
2024-05-13 21:12:43 +0200

    format

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 export default {
-	async fetch(request, env, ctx) {   
-    sendMessage("Hi!",env) 
-    return new Response("I Send you hi!");     
-	}
+	async fetch(request, env, ctx) {
+		sendMessage('Hi!', env);
+		return new Response('I Send you hi!');
+	},
 };
 
-async function sendMessage(message,env) {
-  const response = await fetch('https://api.telegram.org/bot' + env.ENV_BOT_TOKEN + '/sendMessage', {
-      method: 'POST',
-      headers: {  'Content-Type': 'application/json' },
-      body: JSON.stringify({
-          chat_id: env.ENV_BOT_CHATID,
-          text: message.toString().substring(0, 4096)
-      })
-  })  
+async function sendMessage(message, env) {
+	const response = await fetch('https://api.telegram.org/bot' + env.ENV_BOT_TOKEN + '/sendMessage', {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify({
+			chat_id: env.ENV_BOT_CHATID,
+			text: message.toString().substring(0, 4096),
+		}),
+	});
 }
