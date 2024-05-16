3d6f89984657f3aa3ebec002164ec979f9070b79
Ivan Shumilov <ivanshum@mustbefamily.com>
2024-05-16 12:42:16 +0200

    parse CORS docs

---
+++ README.md
@@ -0,0 +1,13 @@
+# Worker resender form data to Telegram via bot
+
+Send a message with data from a form via telegram bot to specified user
+
+## Secret vars
+
+set this vars to secret:
+ENV_BOT_TOKEN=<< Telegram bot token from [@botfather](https://t.me/botfather)>>
+ENV_BOT_CHATID=<< Telegram id of user who will get messages, get it from [@userinfobot](https://t.me/userinfobot)>>
+
+## Evironment vars
+
+set environment var ENV_ALLOW_ORIGIN to your domain
--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 export default {
   async fetch(request, env, ctx) {
+    /** CORS headers */
+    const corsHeaders = {
+      'Access-Control-Allow-Headers': 'X-Requested-With, Content-Type, Accept',
+      'Access-Control-Allow-Methods': 'POST, OPTIONS',
+      'Access-Control-Allow-Origin': env.ENV_ALLOW_ORIGIN,
+    }
+    /** Working with response */
     async function gatherResponse(response) {
       const { headers } = response
       const contentType = headers.get('content-type') || ''
@@ -8,26 +15,43 @@ export default {
       }
       return { contentType, result: response.text() }
     }
-
+    /**
+     * Sends message to Telegram
+     * @param {FormData} body
+     * @returns {Promise} Promise with result of fetch to telegram api
+     */
     async function sendMessage(body) {
-      console.log(body)
+      let msg = ''
+      for (const pair of body.entries()) {
+        msg += pair[0] + ' ' + pair[1] + '\n'
+      }
       return await fetch('https://api.telegram.org/bot' + env.ENV_BOT_TOKEN + '/sendMessage', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
           chat_id: env.ENV_BOT_CHATID,
-          text: 'asd'.substring(0, 4096),
+          text: msg.substring(0, 4096),
         }),
       })
     }
 
-    if (request.method === 'POST' && request.url.indexOf('resend') !== -1) {
-      const resp = await sendMessage(request.body)
+    /** Main code */
+    if (request.method === 'OPTIONS') {
+      return new Response('OK', {
+        headers: corsHeaders,
+      })
+    } else if (request.method === 'POST' && request.url.indexOf('resend') !== -1) {
+      const readbody = new Response(request.body, { headers: request.headers })
+      const formdata = await readbody.formData()
+      const resp = await sendMessage(formdata)
       const { contentType, result } = await gatherResponse(resp)
-      const options = { headers: { 'content-type': contentType } }
+      const options = { headers: { 'content-type': contentType, ...corsHeaders } }
       return new Response(result, options)
     } else {
-      return new Response('Nothing here')
+      return new Response('Method not allowed', {
+        status: 405,
+        headers: corsHeaders,
+      })
     }
   },
 }
